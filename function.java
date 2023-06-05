715fdb936cda436c39e0b92b70dee396aa1a7612
kain-shekar <shekarkain30@gmail.com>
2023-06-05 18:04:29 +0530

    made chnges in code

--- function.java
+++ function.java
@@ -3,24 +3,9 @@
 import java.util.*;
 
 public class function {
-    public static int greater(int x, int y) {
-        if (x > y) {
-            return x;
-        } else {
-            return y;
-        }
-        // return 0;
+public static void main(String [] argue)
+{
+    System.out.println("hello function");
+}
 
-    }
-
-    public static void main(String argue[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter two numbers:");
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c;
-        c = greater(a, b);
-        System.out.println("greater number between " + a + " and " + b + " is :" + c);
-
-    }
 }
